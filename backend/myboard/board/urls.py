d74aa0e0e0ae39618bbec91fd0fd1b245cde8ae4
peachez-z <rkfld0911@gmail.com>
2025-05-06 14:38:08 +0900

    Chore: 서버에서 개발 완료된 코드 초기 커밋

--- backend/myboard/board/urls.py
+++
@@ -1,8 +0,0 @@
-
-from django.urls import path
-from .views import PostListAPIView, PostDetailAPIView
-
-urlpatterns = [
-    path('posts/', PostListAPIView.as_view()),
-    path('posts/<int:pk>/', PostDetailAPIView.as_view()),
-]
